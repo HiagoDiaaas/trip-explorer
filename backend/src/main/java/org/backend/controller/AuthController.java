2d61e800a4d65e4956415084dabf7fa0cea3e3c6
Hiago Dias <51371403+HiagoDiaaas@users.noreply.github.com>
2024-12-18 00:07:53 -0600

    update auth controller

--- backend/src/main/java/org/backend/controller/AuthController.java
+++ backend/src/main/java/org/backend/controller/AuthController.java
@@ -34,7 +34,7 @@ public class AuthController {
                     new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
             User user = (User) authentication.getPrincipal();
             String token = jwtTokenUtil.generateToken(user);
-            return ResponseEntity.ok(new AuthResponse(token, user.getUsername(), user.getName(), user.getId())); 
+            return ResponseEntity.ok(new AuthResponse(token, user.getUsername(), user.getName(), user.getId()));
         } catch (BadCredentialsException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
